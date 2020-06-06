26e7a7a5bc34ba770b321cd3c80357f108d05ca0
shofiyahpeace <shofiyahpeace@gmail.com>
2020-06-06 14:27:36 +0700

    input data timeline

---
+++ app/src/main/java/com/example/myapp/model/activity.java
@@ -0,0 +1,24 @@
+package com.example.myapp.model;
+
+public class activity {
+    String aktivitas;
+    String detailActivity;
+
+    public String getAktivitas() {
+        return aktivitas;
+    }
+
+    public void setAktivitas(String aktivitas) {
+        this.aktivitas = aktivitas;
+    }
+
+    public String getDetailActivity() {
+        return detailActivity;
+    }
+
+    public void setDetailActivity(String detailActivity) {
+        this.detailActivity = detailActivity;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapp/model/activityData.java
@@ -0,0 +1,12 @@
+package com.example.myapp.model;
+
+public class activityData {
+    private  static String [] activity_name = {
+        "membaca",""
+
+    };
+
+    private static String [] activity_detail = {
+        "membaca bla bla",""
+    };
+}
