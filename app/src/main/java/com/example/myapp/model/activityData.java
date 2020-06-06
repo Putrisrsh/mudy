e2521e816d1a6105187aecb40cce224a6ffb1fcf
shofiyahpeace <shofiyahpeace@gmail.com>
2020-06-06 15:23:07 +0700

    input data timeline

--- app/src/main/java/com/example/myapp/model/activityData.java
+++ app/src/main/java/com/example/myapp/model/activityData.java
@@ -2,11 +2,13 @@ package com.example.myapp.model;
 
 public class activityData {
     private  static String [] activity_name = {
-        "membaca",""
+        "Membaca", "Olahraga", "Berkebun", "Memasak", "Menonton Film"
 
     };
 
     private static String [] activity_detail = {
-        "membaca bla bla",""
+        "Yuk membaca buku agar pikiran lebih tenang dan rileks","Jaga kesehatan dengan olahraga ringan agar badan lebih segar",
+            "Rawat tanaman dikebun agar tanaman terawat dengan baik", "Membuat kue atau makanan kesukaan agar mood semakin baik",
+            "Tonton film dengan genre favorit"
     };
 }
